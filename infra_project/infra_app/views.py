3c17374136b5307e219638f4826194d51898f2b5
netzen86 <netzen0su@gmail.com>
2022-09-10 17:32:23 +0500

    изменил views.py

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse(
+        'У меня получилось! залито через github actions'
+    )
 
 
 def second_page(request):
